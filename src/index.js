7d3926e9e7ea2d3f1627168dcf531746263b3704
TBRWEB - Desenvolvimento de Sites <tbrweb01@gmail.com>
2021-12-08 17:28:32 -0300

    Finalização Desafio

--- src/index.js
+++ src/index.js
@@ -10,19 +10,76 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" })
+  }
+
+  request.user = user
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+  
+  if (user.pro) {
+    return next()
+  }
+
+  const totalTodos = user.todos.length
+
+  if (totalTodos < 10) {
+    return next()
+  }
+
+  return response.status(403).json({ error: "Create all ToDo of free plan" })
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+  const idIsValid = validate(id)
+
+  if (!idIsValid) {
+    return response.status(400).json({ error: "Id not valid" })
+  }
+  
+  const user = users.find(user => user.username === username)
+  
+  if (!user) {
+    return response.status(404).json({ error: "User not found" })
+  }
+
+  const todo = user.todos.find(todo => todo.id)
+  
+  if (!todo) {
+    return response.status(404).json({ error: "ToDo not found" })
+  }
+  
+  request.user = user
+  request.todo = todo
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+  const idIsValid = validate(id)
+
+  if (!idIsValid) {
+    return response.status(400).json({ error: "Id not valid" })
+  }
+
+  const user = users.find(user => user.id === id)
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found"})
+  }
+
+  request.user = user
+  return next()
 }
 
 app.post('/users', (request, response) => {
